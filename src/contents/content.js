4aab19528e6497f9a89c8f83ace54a6e3273eb19
phrompong <zphrompong.kh@pttdigital.com>
2022-09-10 20:10:47 +0700

    Create modal for create survey

--- src/contents/content.js
+++ src/contents/content.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import "./content.css";
--- src/popups/createSurvey.js
+++ src/popups/createSurvey.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 const Popup = (props) => {
   const { open, close, detail } = props;
+  const [inputList, setInputList] = useState([]);
 
   const { title } = detail || {};
+
+  useEffect(() => {
+    setInputList([]);
+  }, [close]);
+
+  const addInputList = () => {
+    setInputList([
+      ...inputList,
+      <Form.Control
+        style={{ marginBottom: "5%" }}
+        type="email"
+        placeholder="ตัวเลือก"
+      />,
+    ]);
+  };
 
   return (
     <>
@@ -13,7 +30,22 @@ const Popup = (props) => {
         <Modal.Header closeButton>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body style={{ display: "flex", flexDirection: "column" }}>
+          <Form.Label>หัวข้อ</Form.Label>
+          <Form.Control type="email" placeholder="หัวข้อ" />
+          <Form.Label style={{ paddingTop: "5%" }}>โพล์</Form.Label>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            {inputList}
+            <Button onClick={addInputList} variant="outline-secondary">
+              + เพิ่มตัวเลือก
+            </Button>{" "}
+          </div>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={close}>
             Close
